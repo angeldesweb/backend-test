e6b3cb7c826176f4e46fadf96df06c75f8101cd7
Ángel Zamora <angeldesweb@gmail.com>
2023-05-05 21:10:04 -0400

    docs: :memo: readme added

---
+++ README.md
@@ -0,0 +1,17 @@
+# BACKEND
+
+### Usage
+
+Development mode
+
+```shell
+    npm install
+
+    npm run dev
+```
+
+Queries
+
+GET /products - List of all products
+
+GET /products?filter=name - { found : Expecific matched name , suggested: list by categories}
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,9 +1,15 @@
 import products from '../../data/products.json';
 
 export const getProducts = async (req,res,next) => {
-    const found = products.filter(el => el.name.includes(req.query.filter));
+    let found = [];
+    let suggested = []
+    if(req.query?.filter) {
+        found = products.filter(el => el.name.includes(req.query.filter));
+        suggested = products
+        .filter(el => el.category === found[0].category && JSON.stringify(el) !== JSON.stringify(found[0])).slice(0,2);
 
-    const suggested = products.filter(el => el.category === found[0].category);
+        return res.status(200).send({found:found[0],suggested});
+    }
 
-    return res.status(200).send({found,suggested});
+    return res.status(200).send(products);
 }
